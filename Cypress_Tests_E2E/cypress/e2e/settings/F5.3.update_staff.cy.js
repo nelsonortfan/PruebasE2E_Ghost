c4fbefa06210a560bfa0e92ad06971d8777be243
Nelson <n.ortizf@uniandes.edu.co>
2023-11-18 21:57:46 -0500

    Settings Staff with Screenshot and tested ok

--- Cypress_Tests_E2E/cypress/e2e/settings/F5.3.update_staff.cy.js
+++ Cypress_Tests_E2E/cypress/e2e/settings/F5.3.update_staff.cy.js
@@ -1,3 +1,6 @@
+const { ScreenshotHelper } = require("../../support/utils");
+var path = require('path');
+
 describe("Test set mailgun credentias", () => {
 	
   var userLocation = 'Bogota-Colombia'
@@ -6,28 +9,39 @@ describe("Test set mailgun credentias", () => {
   var validation = '' 	
 	
   beforeEach(() => {
+	cy.viewport(1000, 660);	  
     cy.login();
   });
   
   it("Should set mailgun credentials", () => {
+	  
+		const screenshotTaker = new ScreenshotHelper("settings/F5.3")
     
 		cy.wait(1000)
 		cy.goToPage("settings/");	
-		cy.wait(1000)
+		cy.wait(2000)
+		screenshotTaker.screenshot("Pantalla inicial de settings")
 		cy.contains('Staff').click()
-		cy.wait(1000)
+		cy.wait(2000)
+		screenshotTaker.screenshot("Pantalla inicial de Staff")
 		cy.get('.apps-card-app').click()		
 		cy.wait(1000)
 		cy.get('input[id="user-location"]').clear()
 		cy.get('input[id="user-location"]').type(userLocation)
+		cy.wait(2000)
+		screenshotTaker.screenshot("Pantalla userLocation")
 		cy.get('input[id="user-website"]').clear()
 		cy.get('input[id="user-website"]').type(userWebSite)
+		cy.wait(2000)
+		screenshotTaker.screenshot("Pantalla userWebSite")
 		cy.get('input[id="user-facebook"]').clear()
 		cy.get('input[id="user-facebook"]').type(userFacebook)
+		cy.wait(2000)
+		screenshotTaker.screenshot("Pantalla userFacebook")
 		cy.get('button[data-test-save-button=""]').click()
 		cy.wait(1000)
-		cy.contains('Saved')
-		cy.wait(1000)
+		cy.contains('Saved')		
+		screenshotTaker.screenshot("Pantalla Staff saved")
 		cy.goToPage("settings/");		
 		cy.wait(1000)
 		cy.contains('Staff').click()
@@ -38,14 +52,16 @@ describe("Test set mailgun credentias", () => {
 		validation = $input.val()
 		expect(validation).to.equal(userLocation)	
 		})
+		cy.wait(1000)	
 		cy.get('input[id="user-website"]').should(($input) => {		
 		validation = $input.val()
 		expect(validation).to.equal(userWebSite)	
 		})
+		cy.wait(1000)		
 		cy.get('input[id="user-facebook"]').should(($input) => {		
 		validation = $input.val()
 		expect(validation).to.equal(userFacebook)	
-		})
+		})		
 		cy.wait(1000)
 	
   });
