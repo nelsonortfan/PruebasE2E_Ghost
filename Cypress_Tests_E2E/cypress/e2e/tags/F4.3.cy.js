90e1306fa2fca7a140f55287badce772d7d50360
Angel Racini <angel.racini@mercadolibre.com.co>
2023-11-18 19:06:06 -0500

    screenshot tags

--- Cypress_Tests_E2E/cypress/e2e/tags/F4.3.cy.js
+++ Cypress_Tests_E2E/cypress/e2e/tags/F4.3.cy.js
@@ -1,3 +1,5 @@
+const { ScreenshotHelper } = require("../../support/utils");
+
 describe('Crear un tag, verificar que este listado, luego eliminar ese tag  y  verificar que no siga listado', () => {
     beforeEach(() => {
         cy.login()
@@ -5,33 +7,45 @@ describe('Crear un tag, verificar que este listado, luego eliminar ese tag  y  v
     })
 
     it('Should create tag and then delete it finally proves that is not listed', () => {
+        const screenshotTaker = new ScreenshotHelper("F4.3")
         const tag_name = "Test tag name"
         const tag_description = "test_description"
-        cy.visit('http://localhost:2368/ghost/#')
         cy.contains('Tags').click()
+        screenshotTaker.screenshot("Iniciar eliminar tag")
         cy.contains('New tag').click()
+        screenshotTaker.screenshot("Dar click en crear tag")
         cy.get('#tag-name').type(tag_name)
+        screenshotTaker.screenshot("Rellenar el nombre")
         cy.get('#tag-description').type(tag_description)
+        screenshotTaker.screenshot("LLenar la descripcion")
         cy.contains('Save').click()
+        screenshotTaker.screenshot("guardar exitosamente")
         cy.contains('Tags').click()
+        screenshotTaker.screenshot("Verificar tag este listado")
         const item = cy.contains('h3', 'Test tag name').click();
+        screenshotTaker.screenshot("Ir al detalle del tag")
         // delete the tag
         cy.contains('Delete tag').click()
+        screenshotTaker.screenshot("Dar click en eliminar tag")
+
         //accept the alert in a popup and handle transition aborted error
         cy.on('window:alert', (str) => {
             expect(str).to.equal(`Are you sure you want to delete this tag?`)
         })
+        screenshotTaker.screenshot("Aceptar alerta")
         // delete from modal
         cy.get('button.gh-btn-red').then(($btn) => {
             const button = $btn.get(1)
             button.click()
         })
+        screenshotTaker.screenshot("Eliminar tag")
         cy.on('uncaught:exception', (err, runnable) => {
             if (err.name === 'TransitionAborted') {
                 return false;
             } else {
                 cy.wait(500)
                 cy.visit('http://localhost:2368/ghost/#/tags')
+                screenshotTaker.screenshot("Verificar que no este listado")
                 cy.get('li.gh-list-row.gh-tags-list-item').should('have.length', 0);
                 //verify that the tag is not listed
                 cy.contains('h3', 'Test tag name').should('not.exist')
--- Cypress_Tests_E2E/cypress/e2e/tags/F4.4.cy.js
+++ Cypress_Tests_E2E/cypress/e2e/tags/F4.4.cy.js
@@ -1,3 +1,5 @@
+const { ScreenshotHelper } = require("../../support/utils");
+
 describe('Crear 3 tags, 1 internos y 2 públicos, luego filtrar los tags existentes por tipo y verificar que haya el número correcto por tipo listado', () => {
     beforeEach(() => {
         cy.login()
@@ -5,6 +7,7 @@ describe('Crear 3 tags, 1 internos y 2 públicos, luego filtrar los tags existen
     })
 
     it('Should create 2 internal tag and one public tag the filter and proves that the correct number are listed', () => {
+        const screenshotTaker = new ScreenshotHelper("F4.4")
         const public_tag_name_uno = "Test tag name uno"
         const public_tag_description_uno = "test_description uno"
         const internal_tag_name_uno = "#internal Test tag name uno"
@@ -12,28 +15,44 @@ describe('Crear 3 tags, 1 internos y 2 públicos, luego filtrar los tags existen
         const internal_tag_name_dos = "#internal Test tag name dos"
         const internal_tag_description_dos = "#internal test_description dos"
 
-        cy.visit('http://localhost:2368/ghost/#')
         cy.contains('Tags').click()
+        screenshotTaker.screenshot("Iniciar crear tres tags por tipo")
         cy.contains('New tag').click()
+        screenshotTaker.screenshot("Dar click en crear tag")
         cy.get('#tag-name').type(public_tag_name_uno)
+        screenshotTaker.screenshot("Rellenar el nombre tag publico")
         cy.get('#tag-description').type(public_tag_description_uno)
+        screenshotTaker.screenshot("LLenar la descripcion tag publico")
         cy.contains('Save').click()
+        screenshotTaker.screenshot("guardar exitosamente tag publico")
         cy.contains('Tags').click()
+        screenshotTaker.screenshot("Verificar tag este listado")
         cy.contains('New tag').click()
+        screenshotTaker.screenshot("Dar click en crear tag")
         cy.get('#tag-name').type(internal_tag_name_uno)
+        screenshotTaker.screenshot("Rellenar el nombre tag interno")
         cy.get('#tag-description').type(internal_tag_description_uno)
+        screenshotTaker.screenshot("LLenar la descripcion tag interno")
         cy.contains('Save').click()
+        screenshotTaker.screenshot("guardar exitosamente tag interno")
         cy.contains('Tags').click()
         cy.contains('New tag').click()
+        screenshotTaker.screenshot("Dar click en crear tag")
         cy.get('#tag-name').type(internal_tag_name_dos)
+        screenshotTaker.screenshot("Rellenar el nombre tag interno")
         cy.get('#tag-description').type(internal_tag_description_dos)
+        screenshotTaker.screenshot("LLenar la descripcion tag interno")
         cy.contains('Save').click()
+        screenshotTaker.screenshot("guardar exitosamente tag interno")
         cy.contains('Tags').click()
+        screenshotTaker.screenshot("Verificar tag este listado")
 
         cy.contains('Internal tags').click()
+        screenshotTaker.screenshot("Filtrar por tag interno")
         cy.get('li.gh-list-row.gh-tags-list-item').should('have.length', 2);
 
         cy.contains('Public tags').click()
+        screenshotTaker.screenshot("Filtrar por tag publico")
         cy.get('li.gh-list-row.gh-tags-list-item').should('have.length', 1);
 
     })
