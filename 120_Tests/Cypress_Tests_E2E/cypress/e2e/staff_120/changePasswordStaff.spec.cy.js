361c7dd5b21113c5227fa376500ecfc1859a8d2b
Nelson <n.ortizf@uniandes.edu.co>
2023-11-26 11:08:26 -0500

    Update change password tests and Staff Object

--- 120_Tests/Cypress_Tests_E2E/cypress/e2e/staff_120/changePasswordStaff.spec.cy.js
+++ 120_Tests/Cypress_Tests_E2E/cypress/e2e/staff_120/changePasswordStaff.spec.cy.js
@@ -1,5 +1,4 @@
-import createGhostPage from '../../pageobjects/createGhostPage'
-const { faker } = require("@faker-js/faker");
+import GhostStaff from '../../pageObjects/Staff'
 const csv = require('neat-csv')
 
 const { ScreenshotHelper } = require("../../support/utils");
@@ -8,6 +7,7 @@ var path = require('path');
 let regData
 let numOfPages = 0
 let messageError
+const staffObj = new GhostStaff();	
 
 describe('Prueba de cambio passwords Staff', () => {
 	
@@ -21,120 +21,45 @@ describe('Prueba de cambio passwords Staff', () => {
                 regData = data
            })		
     })
+	
 
-    it('Try to change the passwords that do not match', () => {
-		
+    it('Try to change the passwords that do not match', () => {		
 		cy.wait(1000)
-		cy.goToPage("settings/");	
-		cy.wait(2000)		
-		cy.contains('Staff').click()
-		cy.wait(2000)		
-		cy.get('.apps-card-app').click()		
-		cy.wait(1000)
-		cy.get('input[id="user-password-old"]').click()
-		cy.get('input[id="user-password-old"]').type(regData[0]['curent_password'])
-		cy.wait(1000)
-		cy.get('input[id="user-password-new"]').click()
-		cy.get('input[id="user-password-new"]').type(regData[0]['new_password1'])
-		cy.wait(1000)
-		cy.get('input[id="user-new-password-verification"]').click()
-		cy.get('input[id="user-new-password-verification"]').type(regData[0]['new_password2'])
-		cy.wait(1000)
-		cy.contains('Change Password').click()
-		cy.wait(1000)
+		staffObj.selectOwnerStaff()
+		staffObj.changePasswords(regData[0]['curent_password'], regData[0]['new_password1'], regData[0]['new_password2'])
 		cy.get('p[data-test-error="user-ne2-pass"]').contains("Your new passwords do not match");				
     })	
 	
 	
-	it('Try to change the passwords that are insecures', () => {
-		
+	it('Try to change the passwords that are insecures', () => {		
 		cy.wait(1000)
-		cy.goToPage("settings/");	
-		cy.wait(2000)		
-		cy.contains('Staff').click()
-		cy.wait(2000)		
-		cy.get('.apps-card-app').click()		
-		cy.wait(1000)
-		cy.get('input[id="user-password-old"]').click()
-		cy.get('input[id="user-password-old"]').type(regData[1]['curent_password'])
-		cy.wait(1000)
-		cy.get('input[id="user-password-new"]').click()
-		cy.get('input[id="user-password-new"]').type(regData[1]['new_password1'])
-		cy.wait(1000)
-		cy.get('input[id="user-new-password-verification"]').click()
-		cy.get('input[id="user-new-password-verification"]').type(regData[1]['new_password2'])
-		cy.wait(1000)
-		cy.contains('Change Password').click()
-		cy.wait(1000)
+		staffObj.selectOwnerStaff()
+		staffObj.changePasswords(regData[1]['curent_password'], regData[1]['new_password1'], regData[1]['new_password2'])
 		cy.get('p[data-test-error="user-new-pass"]').contains("Sorry, you cannot use an insecure password.");				
     })
-
-	it('Try to change the passwords with less than 10 characters', () => {
-		
+	
+	
+	it('Try to change the passwords with less than 10 characters', () => {		
 		cy.wait(1000)
-		cy.goToPage("settings/");	
-		cy.wait(2000)		
-		cy.contains('Staff').click()
-		cy.wait(2000)		
-		cy.get('.apps-card-app').click()		
-		cy.wait(1000)
-		cy.get('input[id="user-password-old"]').click()
-		cy.get('input[id="user-password-old"]').type(regData[2]['curent_password'])
-		cy.wait(1000)
-		cy.get('input[id="user-password-new"]').click()
-		cy.get('input[id="user-password-new"]').type(regData[2]['new_password1'])
-		cy.wait(1000)
-		cy.get('input[id="user-new-password-verification"]').click()
-		cy.get('input[id="user-new-password-verification"]').type(regData[2]['new_password2'])
-		cy.wait(1000)
-		cy.contains('Change Password').click()
-		cy.wait(1000)
+		staffObj.selectOwnerStaff()
+		staffObj.changePasswords(regData[2]['curent_password'], regData[2]['new_password1'], regData[2]['new_password2'])
 		cy.get('p[data-test-error="user-new-pass"]').contains("Password must be at least 10 characters long.");				
     })
+	
 
-	it('Try to change the passwords with old password empty', () => {
-		
+	it('Try to change the passwords with old password empty', () => {		
 		cy.wait(1000)
-		cy.goToPage("settings/");	
-		cy.wait(2000)		
-		cy.contains('Staff').click()
-		cy.wait(2000)		
-		cy.get('.apps-card-app').click()		
-		cy.wait(1000)
-		cy.get('input[id="user-password-new"]').click()
-		cy.get('input[id="user-password-new"]').type(regData[3]['new_password1'])
-		cy.wait(1000)
-		cy.get('input[id="user-new-password-verification"]').click()
-		cy.get('input[id="user-new-password-verification"]').type(regData[3]['new_password2'])
-		cy.wait(1000)
-		cy.contains('Change Password').click()
-		cy.wait(1000)
-		cy.get('p[data-test-error="user-old-pass"]').contains("Your current password is required to set a new one");
-		
+		staffObj.selectOwnerStaff()
+		staffObj.changePasswordsWithoutCurrent(regData[3]['new_password1'], regData[3]['new_password2'])
+		cy.get('p[data-test-error="user-old-pass"]').contains("Your current password is required to set a new one");		
     })	
 	
-	it('Try to change the passwords with a wrong old password', () => {
-		
+	
+	it('Try to change the passwords with a wrong old password', () => {		
 		cy.wait(1000)
-		cy.goToPage("settings/");	
-		cy.wait(2000)		
-		cy.contains('Staff').click()
-		cy.wait(2000)		
-		cy.get('.apps-card-app').click()		
-		cy.wait(1000)
-		cy.get('input[id="user-password-old"]').click()
-		cy.get('input[id="user-password-old"]').type(regData[4]['curent_password'])
-		cy.wait(1000)
-		cy.get('input[id="user-password-new"]').click()
-		cy.get('input[id="user-password-new"]').type(regData[4]['new_password1'])
-		cy.wait(1000)
-		cy.get('input[id="user-new-password-verification"]').click()
-		cy.get('input[id="user-new-password-verification"]').type(regData[4]['new_password2'])
-		cy.wait(1000)
-		cy.contains('Change Password').click()
-		cy.wait(1000)
-		cy.contains("Your password is incorrect.");
-		
+		staffObj.selectOwnerStaff()
+		staffObj.changePasswords(regData[4]['curent_password'], regData[4]['new_password1'], regData[4]['new_password2'])
+		cy.contains("Your password is incorrect.");		
     })
 	
 	
--- 120_Tests/Cypress_Tests_E2E/cypress/pageObjects/Staff.js
+++ 120_Tests/Cypress_Tests_E2E/cypress/pageObjects/Staff.js
@@ -1,14 +1,6 @@
 class GhostStaff{
 
-  elements = { 
-       titleInput : () => cy.get('textarea[placeholder="Page title"]'),      
-       descriptionInput : () => cy.get('.koenig-lexical').eq(1),
-	   publishButton : () => cy.contains('Publish'),
-	   continueButton : () => cy.contains('Continue, final review'),
-	   finalPublishButton : () => cy.contains('Publish page, right now')
-   }
-   
-   
+  
    selectOwnerStaff(){
 	    cy.goToPage("settings/");	
 		cy.wait(1000)		
@@ -60,95 +52,36 @@ class GhostStaff{
 	   cy.wait(1000)
    }
    
+   changePasswords(curretPassword, newPassword, validatedPassword){
+	  cy.get('input[id="user-password-old"]').click()
+	  cy.get('input[id="user-password-old"]').type(curretPassword)
+	  cy.wait(1000)
+	  cy.get('input[id="user-password-new"]').click()
+	  cy.get('input[id="user-password-new"]').type(newPassword)
+	  cy.wait(1000)
+	  cy.get('input[id="user-new-password-verification"]').click()
+	  cy.get('input[id="user-new-password-verification"]').type(validatedPassword)
+	  cy.wait(1000)
+	  cy.contains('Change Password').click()
+	  cy.wait(1000) 
+   }
+   
+   changePasswordsWithoutCurrent(newPassword, validatedPassword){
+	  cy.get('input[id="user-password-new"]').click()
+	  cy.get('input[id="user-password-new"]').type(newPassword)
+	  cy.wait(1000)
+	  cy.get('input[id="user-new-password-verification"]').click()
+	  cy.get('input[id="user-new-password-verification"]').type(validatedPassword)
+	  cy.wait(1000)
+	  cy.contains('Change Password').click()
+	  cy.wait(1000) 
+   }
+   
    saveChanges(){
 	   cy.get('button[data-test-save-button=""]').click()
 	   cy.wait(1000)
 	   cy.contains('Saved')
    }
-   
-   
-   
-   createNewPage(title,description){
-	   cy.contains('New page').click();
-	   this.elements.titleInput().type(title);
-	   this.elements.descriptionInput().type(description);
-   }
-   
-   createNewPageWithoutTitle(description){
-	   cy.contains('New page').click();	   
-	   this.elements.descriptionInput().type(description);
-   }
-   
-   publishNewPage(){
-	   this.elements.publishButton().click();
-	   cy.wait(1000);
-	   this.elements.continueButton().click();
-	   cy.wait(1000);
-	   this.elements.finalPublishButton().click();	
-   }
-   
-   get_elementList(index){
-	   return cy.get('.feature-memberAttribution').get('div[role="menuitem"]').eq(index);
-   }
-   
-   updateTitlePage(title_new){
-	   cy.get('textarea[placeholder="Page title"]').clear()		
-	   cy.get('textarea[placeholder="Page title"]').type(title_new)
-	   cy.wait(1000);
-	   cy.contains('Update').click()
-   }
-   
-   settings(){	   
-	   cy.get('button[title="Settings"]').click()
-	   cy.wait(1000)
-   }
-   
-   deletePageConfirmation(){
-	   cy.get('.settings-menu-delete-button').click()
-	   cy.wait(1000)		
-	   cy.get('.modal-content').should('be.visible')
-	   cy.wait(1000)	
-	   cy.get('.modal-footer').get('.gh-btn.gh-btn-red.gh-btn-icon.ember-view').		
-	   contains('Delete').click({force: true})
-   }
-   
-   asociateTag(value){
-	   cy.get('div[id="tag-input"]').click()
-	   cy.get('div[id="tag-input"]').type(value)
-	   cy.get('div[id="tag-input"]').type('{enter}')
-	   cy.get('button[title="Settings"]').click()
-	   cy.wait(1000);
-	   cy.contains('Update').click()
-   }
-   
-   get_valueTag(){
-	   return cy.get('div[id="tag-input"]');
-   }
-   
-   asociateUrl(value){	   
-	   cy.get('.settings-menu-header').should('be.visible')
-	   cy.get('input[id="url"]').should('be.visible')
-	   cy.get('input[id="url"]').clear({force: true})
-	   cy.get('input[id="url"]').click({force: true})
-	   cy.get('input[id="url"]').type(value,{force: true})	   
-	   cy.get('input[id="url"]').type('{enter}',{force: true})
-	   cy.get('button[title="Settings"]').click()
-	   cy.wait(1000);	   
-   }
-   
-   get_valueUrl(){	  
-	   return cy.get('p.ghost-url-preview.description.ember-view');
-   }
-   
-   asociateExcerpt(value){
-	   cy.get('textarea[id="custom-excerpt"]').click({force: true})
-	   cy.get('textarea[id="custom-excerpt"]').type(value, {force: true})
-	   cy.get('textarea[id="custom-excerpt"]').type('{enter}', {force: true})
-	   cy.wait(1000);
-	   cy.get('button[title="Settings"]').click()
-	   cy.wait(1000);	
-   }
-   
    
 }
 
