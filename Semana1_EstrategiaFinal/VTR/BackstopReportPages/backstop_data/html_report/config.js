39c5f5256238c3dcc159fea536073c6b50b84b63
Nelson <n.ortizf@uniandes.edu.co>
2023-12-01 18:20:01 -0500

    Report VTR pages without login

---
+++ Semana1_EstrategiaFinal/VTR/BackstopReportPages/backstop_data/html_report/config.js
@@ -0,0 +1,162 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Semana6GhostV5vsGhostv3_pages_1Pantalla_inicial_de_pagespng_0_document_0_Default.png",
+        "test": "..\\bitmaps_test\\20231201-181551\\Semana6GhostV5vsGhostv3_pages_1Pantalla_inicial_de_pagespng_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Semana6GhostV5vsGhostv3_pages_1Pantalla_inicial_de_pagespng_0_document_0_Default.png",
+        "label": "pages: 1.Pantalla inicial de pages.png",
+        "misMatchThreshold": 0.1,
+        "url": "file:\\\\D:\\Ghost_3.x\\pages\\1.Pantalla inicial de pages.png",
+        "referenceUrl": "file:\\\\D:\\Ghost_5.x\\pages\\1.Pantalla inicial de pages.png",
+        "expect": 0,
+        "viewportLabel": "Default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 3.192853009259259,
+          "misMatchPercentage": "3.19",
+          "analysisTime": 428
+        },
+        "diffImage": "..\\bitmaps_test\\20231201-181551\\failed_diff_Semana6GhostV5vsGhostv3_pages_1Pantalla_inicial_de_pagespng_0_document_0_Default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Semana6GhostV5vsGhostv3_pages_2Pantalla_data_de_pagepng_0_document_0_Default.png",
+        "test": "..\\bitmaps_test\\20231201-181551\\Semana6GhostV5vsGhostv3_pages_2Pantalla_data_de_pagepng_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Semana6GhostV5vsGhostv3_pages_2Pantalla_data_de_pagepng_0_document_0_Default.png",
+        "label": "pages: 2.Pantalla data de page.png",
+        "misMatchThreshold": 0.1,
+        "url": "file:\\\\D:\\Ghost_3.x\\pages\\2.Pantalla data de page.png",
+        "referenceUrl": "file:\\\\D:\\Ghost_5.x\\pages\\2.Pantalla data de page.png",
+        "expect": 0,
+        "viewportLabel": "Default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 4.452449845679012,
+          "misMatchPercentage": "4.45",
+          "analysisTime": 277
+        },
+        "diffImage": "..\\bitmaps_test\\20231201-181551\\failed_diff_Semana6GhostV5vsGhostv3_pages_2Pantalla_data_de_pagepng_0_document_0_Default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Semana6GhostV5vsGhostv3_pages_3Pantalla_Header_y_Footer_de_pagepng_0_document_0_Default.png",
+        "test": "..\\bitmaps_test\\20231201-181551\\Semana6GhostV5vsGhostv3_pages_3Pantalla_Header_y_Footer_de_pagepng_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Semana6GhostV5vsGhostv3_pages_3Pantalla_Header_y_Footer_de_pagepng_0_document_0_Default.png",
+        "label": "pages: 3.Pantalla Header y Footer de page.png",
+        "misMatchThreshold": 0.1,
+        "url": "file:\\\\D:\\Ghost_3.x\\pages\\3.Pantalla Header y Footer de page.png",
+        "referenceUrl": "file:\\\\D:\\Ghost_5.x\\pages\\3.Pantalla Header y Footer de page.png",
+        "expect": 0,
+        "viewportLabel": "Default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 3.2169656635802473,
+          "misMatchPercentage": "3.22",
+          "analysisTime": 270
+        },
+        "diffImage": "..\\bitmaps_test\\20231201-181551\\failed_diff_Semana6GhostV5vsGhostv3_pages_3Pantalla_Header_y_Footer_de_pagepng_0_document_0_Default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Semana6GhostV5vsGhostv3_pages_4Pantalla_principal_con_la_page_creadapng_0_document_0_Default.png",
+        "test": "..\\bitmaps_test\\20231201-181551\\Semana6GhostV5vsGhostv3_pages_4Pantalla_principal_con_la_page_creadapng_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Semana6GhostV5vsGhostv3_pages_4Pantalla_principal_con_la_page_creadapng_0_document_0_Default.png",
+        "label": "pages: 4.Pantalla principal con la page creada.png",
+        "misMatchThreshold": 0.1,
+        "url": "file:\\\\D:\\Ghost_3.x\\pages\\4.Pantalla principal con la page creada.png",
+        "referenceUrl": "file:\\\\D:\\Ghost_5.x\\pages\\4.Pantalla principal con la page creada.png",
+        "expect": 0,
+        "viewportLabel": "Default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 4.465808256172839,
+          "misMatchPercentage": "4.47",
+          "analysisTime": 290
+        },
+        "diffImage": "..\\bitmaps_test\\20231201-181551\\failed_diff_Semana6GhostV5vsGhostv3_pages_4Pantalla_principal_con_la_page_creadapng_0_document_0_Default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Semana6GhostV5vsGhostv3_pages_5Pantalla_despues_de_logoutpng_0_document_0_Default.png",
+        "test": "..\\bitmaps_test\\20231201-181551\\Semana6GhostV5vsGhostv3_pages_5Pantalla_despues_de_logoutpng_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Semana6GhostV5vsGhostv3_pages_5Pantalla_despues_de_logoutpng_0_document_0_Default.png",
+        "label": "pages: 5.Pantalla despues de logout.png",
+        "misMatchThreshold": 0.1,
+        "url": "file:\\\\D:\\Ghost_3.x\\pages\\5.Pantalla despues de logout.png",
+        "referenceUrl": "file:\\\\D:\\Ghost_5.x\\pages\\5.Pantalla despues de logout.png",
+        "expect": 0,
+        "viewportLabel": "Default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 3.70703125,
+          "misMatchPercentage": "3.71",
+          "analysisTime": 329
+        },
+        "diffImage": "..\\bitmaps_test\\20231201-181551\\failed_diff_Semana6GhostV5vsGhostv3_pages_5Pantalla_despues_de_logoutpng_0_document_0_Default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Semana6GhostV5vsGhostv3_pages_6Pantalla_visita_de_la_pagepng_0_document_0_Default.png",
+        "test": "..\\bitmaps_test\\20231201-181551\\Semana6GhostV5vsGhostv3_pages_6Pantalla_visita_de_la_pagepng_0_document_0_Default.png",
+        "selector": "document",
+        "fileName": "Semana6GhostV5vsGhostv3_pages_6Pantalla_visita_de_la_pagepng_0_document_0_Default.png",
+        "label": "pages: 6.Pantalla visita de la page.png",
+        "misMatchThreshold": 0.1,
+        "url": "file:\\\\D:\\Ghost_3.x\\pages\\6.Pantalla visita de la page.png",
+        "referenceUrl": "file:\\\\D:\\Ghost_5.x\\pages\\6.Pantalla visita de la page.png",
+        "expect": 0,
+        "viewportLabel": "Default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 21.95558449074074,
+          "misMatchPercentage": "21.96",
+          "analysisTime": 282
+        },
+        "diffImage": "..\\bitmaps_test\\20231201-181551\\failed_diff_Semana6GhostV5vsGhostv3_pages_6Pantalla_visita_de_la_pagepng_0_document_0_Default.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "Semana 6 GhostV5 vs Ghostv3"
+});
